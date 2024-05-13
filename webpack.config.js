1b18bc8fbc918bbd417af1b78fdbe0276ee59126
Ibrahim Salah <wwwe@me.com>
2024-05-13 16:32:03 +0300

    start working with CSS as per EPM

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,27 @@
-const path = require( 'path')
+const path = require("path")
 
-module. exports = {
-  entry: './app/assets/scripts/App.js',
-output: {
-  filename: 'bundled.js',
-  path: path.resolve( __dirname, 'app')
-},
+const postCSSPlugins =
+[
+require('postcss-import'),
+require("postcss-simple-vars"), 
+require("postcss-nested"), 
+require("autoprefixer"),
+]
 
-mode: 'development',
-watch: true,
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    filename: "bundled.js",
+    path: path.resolve(__dirname, "app")
+  },
+  mode: "development",
+  watch: true,
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", { loader: "css-loader", options: { url: false } }, { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
+      }
+    ]
+  }
 }
